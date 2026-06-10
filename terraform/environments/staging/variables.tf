fe9ef259761407db13d28a59fe1fd7c00e6dcb5f
ca229863 <ca229863@fi365.ort.edu.uy>
2026-06-10 00:04:03 -0300

    terraform

--- terraform/environments/staging/variables.tf
+++ terraform/environments/staging/variables.tf
@@ -1,17 +1,61 @@
 variable "aws_region" {
-  description = "Region de AWS"
-  type        = string
-  default     = "us-east-1"
+  type    = string
+  default = "us-east-1"
 }
 
 variable "project_name" {
-  description = "Nombre del proyecto"
-  type        = string
-  default     = "cipolflo"
+  type    = string
+  default = "cipolflo"
 }
 
 variable "environment" {
-  description = "Ambiente"
-  type        = string
-  default     = "staging"
+  type    = string
+  default = "staging"
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "db_username" {
+  type = string
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "auth0_issuer_uri" {
+  type = string
+}
+
+variable "auth0_audience" {
+  type = string
+}
+
+variable "auth0_domain" {
+  type = string
+}
+
+variable "auth0_client_id" {
+  type = string
+}
+
+variable "certificate_arn" {
+  type = string
+}
+
+variable "hosted_zone_id" {
+  type = string
+}
+
+variable "domain_name" {
+  type    = string
+  default = "cipolflo.com.uy"
+}
+
+variable "subdomain" {
+  type    = string
+  default = "staging"
 }
---
+++ terraform/modules/rds/variables.tf
@@ -0,0 +1,38 @@
+variable "project_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
+}
+
+variable "rds_security_group_id" {
+  type = string
+}
+
+variable "db_instance_class" {
+  type    = string
+  default = "db.t3.micro"
+}
+
+variable "db_allocated_storage" {
+  type    = number
+  default = 20
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "db_username" {
+  type = string
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
